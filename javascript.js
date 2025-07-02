ebaada04c40a6d83f959b1dc6a7136315ddee112
winter-souls <183771291+winter-souls@users.noreply.github.com>
2025-07-02 07:50:41 -0700

    Delete consts
    Pass getHumanChoice() and getComputerChoice() to playRound as arguments

---
+++ javascript.js
@@ -0,0 +1,39 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+
+    if (randomNumber <= 0.3) {
+        computerChoice = "Rock";
+    } else if (randomNumber <= 0.6) {
+        computerChoice = "Paper";
+    } else {
+        computerChoice = "Scissors";
+    }
+
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    let humanChoice = window.prompt("Choose: rock, paper, or scissors?");
+    //console.log(humanChoice);
+    return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    //switch
+    //case player = paper
+    //if PC chose paper = tie, else if PC chose scissor = lose, else if PC chose rock = win
+
+    //case player = scissor
+    //same shit (increment score variables accordingly)
+
+    //case player = rock
+    //same shit
+    console.log(humanChoice);
+    console.log(computerChoice);
+}
+
+//playRound(getHumanChoice(), getComputerChoice());
+
