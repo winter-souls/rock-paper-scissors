0f6a8e3e4ad8a24377982b03719eb341091ffb69
winter-souls <183771291+winter-souls@users.noreply.github.com>
2025-07-04 08:06:31 -0700

    Increment the scores based on winner each round
    Comment in explanations of the code

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
+//score variables
 let humanScore = 0;
 let computerScore = 0;
 
+//computer chooses rock/paper/scissors based on a random decimal point value
 function getComputerChoice() {
     let randomNumber = Math.random();
 
@@ -15,18 +17,23 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+//human gets window prompt that asks them to choose R/P/S
 function getHumanChoice() {
     let humanChoice = window.prompt("Choose: rock, paper, or scissors?");
     return humanChoice;
 }
 
+//plays one round of R/P/S
 function playRound(humanChoice, computerChoice) {
+    //switch all human inputs to upper case for easy comparison
     const playerChoice = humanChoice.toUpperCase()
+
     //win conditions for player
     if (playerChoice == "ROCK" && computerChoice == "Scissors" ||
     playerChoice == "SCISSORS" && computerChoice == "Paper" ||
     playerChoice == "PAPER" && computerChoice == "Rock") {
         console.log(`You won! ${humanChoice} beats ${computerChoice}!`);
+        humanScore++
 
         //tie conditions
      } else if (playerChoice == "ROCK" && computerChoice == "Rock" ||
@@ -40,7 +47,8 @@ function playRound(humanChoice, computerChoice) {
             playerChoice == "SCISSORS" && computerChoice == "Rock" ||
             playerChoice == "PAPER" && computerChoice == "Scissors"
         ){
-            console.log(`You lose! ${humanChoice} succumbs to ${computerChoice}.`)
+            console.log(`You lose! ${humanChoice} succumbs to ${computerChoice}.`);
+            computerScore++
     }
 }
 
